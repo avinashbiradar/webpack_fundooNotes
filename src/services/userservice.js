f107f621d83eb5943e4fad45d756e032e0654d94
avinashbiradar <avinashbiradar179@gmail.com>
2021-02-26 15:26:13 +0530

    Added:api calling for edit note , update note , display all notes

--- src/services/userservice.js
+++ src/services/userservice.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = `http://fundoonotes.incubation.bridgelabz.com/api/user`;
-const Url=`http://fundoonotes.incubation.bridgelabz.com/api/`;
+const Url = `http://fundoonotes.incubation.bridgelabz.com/api/`;
 export function login(data) {
     console.log("This is the Sign in part", data);
     let data2 = axios.post(`${baseUrl}/login`, data);
@@ -14,10 +14,10 @@ export default function registration(data) {
     return data1;
 };
 
-export function resetPass(data,id) {
-    console.log("This is reset password part", data,id);
-    let data3 = axios.post(`${baseUrl}/reset-password`,data,{headers:{'Authorization':id}});
-    return data3;   
+export function resetPass(data, id) {
+    console.log("This is reset password part", data, id);
+    let data3 = axios.post(`${baseUrl}/reset-password`, data, { headers: { 'Authorization': id } });
+    return data3;
 };
 export function forgotPass(data) {
     console.log("This is forgot password part", data);
@@ -25,29 +25,18 @@ export function forgotPass(data) {
     return data3;
 
 };
-// export function addNote(data){
-//     console.log("this is take a note ")
-//         return axios.post(`${Url}/notes/addNotes`,data,{headers:{'Authorization':localStorage.getItem('token')}})
-
-// }
-// export function getNotes(){
-//     console.log("this is get all notes ")
-//         return axios.post(`${Url}/notes/getNotesList`,{headers:{'Authorization':localStorage.getItem('token')}})
-
-// }
-
-// export function updateNotes(formData)  {
-//         return axios.post(`${Url}/notes/updateNotes`,formData,{headers:{'Authorization':localStorage.getItem('token')}})
-// }
-export function addNote(data){
-        return axios.post(`http://fundoonotes.incubation.bridgelabz.com/api/notes/addNotes`,data,{headers:{'Authorization':localStorage.getItem('token')}})
+export function addNote(data) {
+    console.log("this is take a note ")
+    return axios.post(`${Url}/notes/addNotes`, data, { headers: { 'Authorization': localStorage.getItem('token') } })
 
 }
 
-export function getNotes(){
-    return axios.get(`http://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList`,{headers:{'Authorization':localStorage.getItem('token')}})
-  };
-
-export function updateNotes(formData)  {
-        return axios.post(`http://fundoonotes.incubation.bridgelabz.com/api/notes/updateNotes`,formData,{headers:{'Authorization':localStorage.getItem('token')}})
+export function updateNotes(formData) {
+    return axios.post(`${Url}/notes/updateNotes`, formData, { headers: { 'Authorization': localStorage.getItem('token') } })
 }
+
+export function getNotes() {
+    let data4= axios.get(`${Url}/notes/getNotesList`, { headers: { 'Authorization': localStorage.getItem('token') } })
+    return data4;
+};
+
